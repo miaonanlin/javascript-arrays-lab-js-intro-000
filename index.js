23dad396eb0705042135d59603cc609d128f4622
Miao Nan Lin <miaonanlin@gmail.com>
2020-03-31 20:52:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ var kittens = ["Milo","Otis","Garfield"]
  //define your array here
 // Add your functions and code here
 function destructivelyAppendKitten(name){
-  return kittens.push;
+  return kittens.push()
 }
+
+
 
 function destructivelyPrependKitten(name){
   return kittens.unshift(name);
 }
+
+
 
 function destructivelyRemoveLastKitten(){
   return kittens.pop();
