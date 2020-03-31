2767fd027c02516512b87c158cd5c1a7fad75429
Miao Nan Lin <miaonanlin@gmail.com>
2020-03-31 20:32:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var kittens = ["Milo","Otis","Garfield"]
 // Add your functions and code here
 function destructivelyAppendKitten(name){
   kittens.push(name)
-  return kittens
+  return kittens;
 }
 
 function destructivelyAppendKitten(name){
